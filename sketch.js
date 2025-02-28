780fd4aa9646185a3beb9819ffcc1b46861c875c
EugenioSC <147703762+EugenioSC@users.noreply.github.com>
2025-02-28 09:24:58 +0000

    VersionNoT

--- sketch.js
+++ sketch.js
@@ -1,14 +1,23 @@
+let gatito
+let gatito2
+
 function preload() {
   // put preload code here
+  gatito = loadImage('./images/gatito.jpg')
+  gatito2 = loadImage('./images/gatito.jpg')
+
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800,800)
+  createCanvas(gatito.width,gatito.height)
+  noLoop()
 }
 
 function draw() {
   // put drawing code here
   background(255)
-  rect(100,100,200,200)
+  let numPixeles = 4*gatito.width*gatito.height
+  for (let i=0; i<numPixeles; i++){}
+  image(gatito2,0,0,300,300)
 }
